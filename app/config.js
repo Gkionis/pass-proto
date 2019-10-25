defa8d4edc687dfb57b94d361c2dcb39ecc5de52
OtusScops <13008829+Gkionis@users.noreply.github.com>
2019-10-25 10:41:08 +0100

    daily commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Academy Transfers',
+  serviceName: 'Apply to transfer an Academy',
 
   // Default port that prototype runs on
   port: '3000',
