a442a1b553e42f6e6c48b7f298961d6afe87b270
OtusScops <13008829+Gkionis@users.noreply.github.com>
2019-11-25 16:10:37 +0000

    Service name change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply to transfer an Academy',
+  serviceName: 'Academy Transfers design history',
 
   // Default port that prototype runs on
   port: '3000',
